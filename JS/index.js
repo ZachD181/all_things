e5c0b1416a1a276c1d3c6a73343b7ff04b3f057f
ZachD181 <84939280+ZachD181@users.noreply.github.com>
2022-01-08 09:36:23 -0700

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,40 @@
+console.log("Hello world");
+var location = "I'm going" + "to the store";
+var string1 = "time to";
+var string2 = "go tobed";
+var string3 = string1 + string2;
+console.log(string3);
+
+
+// a. Add
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z);
+// b. subtract
+
+var s = 6;
+var d = 7;
+var l = s - d;
+console.log(l);
+//c. divide
+var a = 1
+var b = 3
+var c = a / b;
+console.log(c);
+// d. multiply
+var e = 1
+var f = 3
+var g = a * b;
+console.log(g);
+//2. Strings
+//a.
+var string = "Hello";
+var string4 = "World";
+var string5 = string + string4;
+console.log(string5);
+//b.
+var string1 = "my favorite food is";
+var string2 = "avacado";
+var string3 = string1 + string2;
+console.log(string3);
